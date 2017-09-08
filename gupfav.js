326497b0a18276944e970d5dfb41abeca7e32644
Nataraj <atchula.nataraj@live.in>
2017-09-08 12:32:05 +0530

    Create gupfav.js

---
+++ gupfav.js
@@ -0,0 +1,16 @@
+function guper(name, link) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(link);
+        if (results == null) return null;
+        else return results[1];
+    }
+        function gup(name) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(window.location.href);
+        if (results == null) return null;
+        else return results[1];
+    }
